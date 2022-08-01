334ffb30a880ef9a81446b9320510f0f004d4411
Kirill L <thearrex@gmail.com>
2022-08-01 17:54:42 +0700

    vue init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <div class="wrapper">
+
+    </div>
+  </header>
+
+  <main>
+    <IconFile/>
+    <IconFolder/>
+    <IconPencil/>
+    <IconTrash/>
+  </main>
+</template>
+
+<script setup>
+import IconFile from './components/icons/IconFile.vue'
+import IconFolder from './components/icons/IconFolder.vue'
+import IconPencil from './components/icons/IconPencil.vue'
+import IconTrash from './components/icons/IconTrash.vue'
+
+</script>
